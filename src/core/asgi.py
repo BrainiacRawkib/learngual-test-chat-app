e78812b320e9eb3cd04f223dc5fa01ef9f7ffba7
Brainiac Rawkib <brainiacrawkib@gmail.com>
2024-03-05 17:34:14 +0100

    updates core.asgi

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -13,14 +13,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from apis.chat.routing import websocket_urlpatterns
-from apis.authentication.middleware import WebSocketJWTAuthMiddleware
-
 
 # set the environment settings before calling any application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'core.settings')
 
 django_asgi_app = get_asgi_application()
+
+from apis.chat.routing import websocket_urlpatterns
+from apis.authentication.middleware import WebSocketJWTAuthMiddleware
+
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
